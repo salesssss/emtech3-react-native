d4e285c7db289fb23ee895675bb4221506f0be23
salesssss <112595047+salesssss@users.noreply.github.com>
2025-04-01 16:37:56 +0800

    Add files via upload

---
+++ BasicModal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicModal from './components/BasicModal'; // Adjust the path if needed
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <BasicModal /> {/* This will render the button that opens the modal */}
+        </div>
+    );
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Navbar = () => (
+    <AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="user icon">
+                <AccountCircleIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Goal List
+            </Typography>
+        </Toolbar>
+    </AppBar>
+);
+
+export default Navbar;
